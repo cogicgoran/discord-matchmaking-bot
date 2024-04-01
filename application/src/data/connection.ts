40dd6095b7fc60784f5c40053b33bfcfa1e6a4e1
cogicgoran <cogicgoran99@gmail.com>
2024-04-01 15:05:44 +0200

    Refactor database connection

--- application/src/data/connection.ts
+++ application/src/data/connection.ts
@@ -1,16 +1,17 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import config from '../config';
 
-const client = new MongoClient(config.env.MONGO_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+let client: MongoClient;
 
 export async function initDatabase() {
   try {
+    client = new MongoClient(config.env.MONGO_URI, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
     await client.connect();
   } catch (error) {
     console.log('[DatabaseConnectionError]:', error);
@@ -18,4 +19,4 @@ export async function initDatabase() {
   }
 }
 
-export default client;
+export default client!;
